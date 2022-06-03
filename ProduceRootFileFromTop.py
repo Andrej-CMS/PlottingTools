90c92bd123c3eec664f91413d3a2c49b1a97a22d
Andrej Saibel <asaibel@pflc20.ific.uv.es>
2022-06-03 12:22:39 +0200

    script to convert histo text files into root for further analysis. Takes a list of comma separated input files and writes into a TList object

--- ProduceRootFileFromTop.py
+++ ProduceRootFileFromTop.py
@@ -6,7 +6,7 @@ import ROOT.TH2D as TH2D
 import ROOT.TGraph2D as TGraph2D
 from ROOT import TCanvas, TColor, TGaxis, TH1F, TPad, TF1, TString
 from ROOT import kBlack, kGreen, kRed
-from ROOT import TFile, gStyle, TRatioPlot, gPad, TGraphErrors, TGraphAsymmErrors
+from ROOT import TFile, gStyle, TRatioPlot, gPad, TGraphErrors, TGraphAsymmErrors, TObject
 import math
 import array
 import glob
@@ -22,13 +22,26 @@ from plottingUtils import *
 if __name__ == '__main__':
     
     parser = OptionParser()
-    parser.add_option("-f", "--file", dest="filename",
+    parser.add_option("-f", "--file", dest="filename", 
                     help="Input file name", metavar="FILE")
     parser.add_option("-o", "--output", dest="outputFile",
                     help="Output file name", metavar="FILE")
 
     (options, args) = parser.parse_args()
     
+    outputPath = "HistoListsFromTextFiles"
+    outputFile = options.outputFile
+    debug = False
+    
+    fileNameList = options.filename.split(",")
+    
+    binCenters = []
+    binContent = [] 
+    binUncertainties = []
+    histoList = []
+    
+    fileContentDictionary = {}
+
     #read input files
     #all input files should be given
     #the script creates all histograms in one single file
@@ -37,10 +50,53 @@ if __name__ == '__main__':
     #read in all lines from input top files
     #structure is: [bin-center, bin-content, bin-uncertainties]
     #fill content into dictionary
+    for fileName in fileNameList:
+        with open(fileName) as file:
+            del binCenters[:]
+            del binContent[:]
+            del binUncertainties[:]
+
+            for line in file:
+                lineContent = line.split()
+                
+                binCenters.append(float(lineContent[0]))
+                binContent.append(float(lineContent[1]))
+                binUncertainties.append(float(lineContent[2]))
+                
+            fileContentDictionary.update({fileName : [binCenters, binContent, binUncertainties]})
+                
+    
+    if debug:
+        print(fileContentDictionary)
+        print(binCenters)
+        print(binContent)
+        print(binUncertainties)
     
     #contruct ROOT TH1 histograms from input dictionary
     #Name of the histogram: mass_PDF_scale_distribution
+    for key in fileContentDictionary:
+        fileName = makeFileNameFromPath(key)
+        
+        histoList.append(makeHistoFromLists(fileContentDictionary.get(key)[0], 
+                                            fileContentDictionary.get(key)[1],
+                                            fileContentDictionary.get(key)[2], 
+                                            fileName))
+    #checked manually that the numbers are exactly the same as in the input files
+    if debug:
+        for xBin in range(histoList[0].GetNbinsX()):
+            print(xBin, histoList[0].GetBinCenter(xBin))
           
     #create output folder and write output file
-    
+    outputList = TList()
+    for histo in histoList:
+        outputList.Add(histo)
+        
+    if not os.path.exists(outputPath):
+        # Create a new directory because it does not exist 
+        os.makedirs(outputPath)
+        print("The new directory is created!")
+        
+    outputFile = TFile(outputPath+"/"+outputFile+".root","RECREATE");
+    outputList.Write("histoList", TObject.kSingleKey);
+    outputFile.Close()
     
